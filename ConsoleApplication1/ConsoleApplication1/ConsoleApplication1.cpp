0aac13177f9614e26a04d8aae38fbd2c06cacf55
MaxTrgb <126002400+MaxTrgb@users.noreply.github.com>
2023-06-16 18:10:29 +0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,48 @@
+﻿/*Given a text file. Calculate the length of the shortest word in this file 
+(a word is a group of consecutive characters other than spaces and punctuation marks).*/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    ifstream read;
+    read.open("Task4.txt"); // Open the input file named "Task4.txt"
+
+    if (!read.is_open()) {
+
+        cout << "Error" << endl; // Display an error message if the file couldn't be opened
+        return 1; // Return from the program with an error code
+    }
+
+    char element;
+    int counter = 0;
+    int min_word = INT_MAX; // Initialize the minimum word length to the maximum possible value
+
+    while (read.get(element)) { // Read characters from the file until the end
+
+        if (element != '\n' && element != ',' && element != '.' && element != ' ') {
+
+            // If the character is not a newline, comma, dot, or space, it belongs to a word
+            counter++; // Increment the word length counter
+        }
+        else {
+
+            // If a word boundary is reached (newline, comma, dot, or space)
+            if (counter < min_word && counter > 0) {
+
+                // If the current word length is shorter than the minimum word length so far (excluding empty words)
+                min_word = counter; // Update the minimum word length
+            }
+
+            counter = 0; // Reset the word length counter for the next word
+        }
+    }
+
+    cout << min_word << endl; // Print the length of the shortest word in the file
+
+    return 0;
+}
---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,183 @@
+﻿/*Create a text file that contains positive, negative, zero numbers and arbitrary characters. 
+Determine the number of positive, negative, zero numbers and words in each line of the file. 
+Write the received values with appropriate comments in another text file.*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <cctype>
+#include <algorithm>
+
+using namespace std;
+
+// Function to count the number of positive, negative, and zero numbers in a line
+void countNumbers(const string& line, int& positiveCount, int& negativeCount, int& zeroCount) {
+
+    istringstream iss(line);
+    string word;
+    while (iss >> word) {
+
+        // Remove commas from the word
+        word.erase(remove(word.begin(), word.end(), ','), word.end());
+
+        bool isNumber = true;
+
+        // Check if the word is a number
+        int decimalCount = 0;  // Count of decimal points
+        for (char ch : word) {
+
+            if (!isdigit(ch) && ch != '-' && ch != '.') {
+
+                isNumber = false;
+                break;
+            }
+            if (ch == '.') {
+
+                decimalCount++;
+            }
+        }
+
+        if (isNumber && decimalCount <= 1) {
+
+            double number = stod(word);
+
+            if (number > 0) {
+
+                positiveCount++;
+            }
+            else if (number < 0) {
+
+                negativeCount++;
+            }
+            else {
+                zeroCount++;
+            }
+        }
+    }
+}
+
+// Function to count the number of words in a line
+int countWords(const string& line) {
+
+    istringstream iss(line);
+    string word;
+    int wordCount = 0;
+
+    while (iss >> word) {
+
+        bool isWord = false;
+        for (int i = 0; i < word.length(); i++) {
+
+            if (isalpha(word[i])) {
+
+                isWord = true;
+                break;
+            }
+        }
+        if (isWord) {
+
+            wordCount++;
+        }
+    }
+    return wordCount;
+}
+
+// Function to display the contents of a file
+void displayFileContents(const string& filename) {
+
+    ifstream file(filename);
+    if (file) {
+
+        string line;
+        while (getline(file, line)) {
+
+            cout << line << endl;
+        }
+        file.close();
+    }
+    else {
+
+        cout << "Failed to open the file." << endl;
+    }
+}
+
+int main() {
+    ofstream inputFile("input.txt"); // File to store the input data
+
+    // Check if the input file was successfully opened
+    if (!inputFile) {
+
+        cout << "Failed to open the input file." << endl;
+        return 1;
+    }
+
+    string line;
+    cout << "Enter the data (press Enter on an empty line to finish):\n";
+    while (getline(cin, line) && !line.empty()) {
+
+        inputFile << line << endl; // Write the line to the input file
+    }
+
+    // Close the input file
+    inputFile.close();
+
+    cout << "Input file created. You can find it in the current directory with the name input.txt." << endl;
+
+    ifstream read("input.txt"); // Open the input file for reading
+
+    if (!read.is_open()) {
+
+        cout << "Failed to open the input file." << endl;
+        return 1;
+    }
+
+    ofstream outputFile("results.txt"); // File to store the results and commentaries
+
+    // Check if the output file was successfully opened
+    if (!outputFile) {
+
+        cout << "Failed to open the output file." << endl;
+        return 1;
+    }
+
+    while (getline(read, line)) {
+
+        int positiveCount = 0;
+        int negativeCount = 0;
+        int zeroCount = 0;
+
+        // Count the number of positive, negative, and zero numbers in the line
+        countNumbers(line, positiveCount, negativeCount, zeroCount);
+
+        // Count the number of words in the line
+        int wordCount = countWords(line);
+
+        // Write results with appropriate comments to the output file
+        outputFile << "Line: " << line << endl;
+        outputFile << "Positive numbers: " << positiveCount << endl;
+        outputFile << "Negative numbers: " << negativeCount << endl;
+        outputFile << "Zero numbers: " << zeroCount << endl;
+        outputFile << "Word count: " << wordCount << endl;
+        outputFile << "------------------------------" << endl;
+    }
+
+    // Close the input and output files
+    read.close();
+    outputFile.close();
+
+    cout << "Counting completed. Results and commentaries are written to results.txt." << endl;
+
+    // Prompt the user to view the results file
+    char choice;
+    cout << "Do you want to view the results file? (Y/N): ";
+    cin >> choice;
+
+    if (choice == 'Y' || choice == 'y') {
+
+        cout << "Contents of results.txt:\n";
+        displayFileContents("results.txt");
+    }
+
+    return 0;
+}
